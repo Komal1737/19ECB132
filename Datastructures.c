2ee164183a3a1c25b79d11901f9a0bb84027fa68
Komal <101517209+Komal1737@users.noreply.github.com>
2022-05-31 18:01:07 +0000

    output

--- Datastructures.c
+++ Datastructures.c
@@ -1,96 +1,157 @@
 # include <stdio.h>
-# include <stdlib.h>
+# define SIZE 5
 
-int choice;
 
-struct singly_node{
-  int data;
-   struct singly_node *next;
-};
-
-struct doubly_node{
-  int data;
-   struct doubly_node *next, *prev;
-};
+int array_data[SIZE];
+int top_of_stack, front, rear;
 
 void menu(void) {
-  printf("\n1. Singly Linked List \n2. Doubly Linked List \n3. Circular Linked List \n4. Exit");
-  printf("\nChoose an option: ");
-  scanf("%d", &choice);
+  printf("\n1. Stack using Array");
+  printf("\n2. Queue using Array");
+  printf("\n3. Stack using Linked List");
+  printf("\n4. Queue using Linked List");
+  printf("\n5. Exit from the program");
+  printf("\n\n Provide an option: ");
 }
 
-void singly_linked_list(){
-  printf("\n*** Singly Linked List ***");
-  int choice = 1;
-  struct singly_node *head = NULL, *new_node, *temp;
-  while (choice == 1) {
-    new_node = (struct singly_node *)malloc(sizeof(struct singly_node));
-    printf("\nEnter node data: ");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
-    if (head == NULL)
-      head = temp = new_node;
-    else {
-      temp->next = new_node;
-      temp = new_node;
-    }
-    printf("\nTo insert a new node press 1 else any other integer: ");
-    scanf("%d", &choice);
-  } 
-
-  printf("\nThe elements in the Singly Linked list are...");
-  temp = head;
-  while (temp != NULL) {
-    printf("\n%d", temp->data);
-    temp = temp->next;
+void push(void) {
+  if (top_of_stack == SIZE) 
+    printf("\nStack Overflow");
+  else {
+    printf("\nEnter the element to be pushed to the stack: ");
+    scanf("%d", &array_data[top_of_stack++]);
   }
 }
 
-void doubly_linked_list(){
-// Incomplete code
-  printf("\n*** Doubly Linked List ***");
-  int choice = 1;
-  struct doubly_node *head = NULL, *new_node, *temp;
-  while (choice == 1) {
-    new_node = (struct singly_node *)malloc(sizeof(struct singly_node));
-    printf("\nEnter node data: ");
-    scanf("%d", &new_node->data);
-    new_node->next = NULL;
-    if (head == NULL)
-      head = temp = new_node;
-    else {
-      temp->next = new_node;
-      temp = new_node;
-    }
-    printf("\nTo insert a new node press 1 else any other integer: ");
-    scanf("%d", &choice);
-  } 
-
-  printf("\nThe elements in the Singly Linked list are...");
-  temp = head;
-  while (temp != NULL) {
-    printf("\n%d", temp->data);
-    temp = temp->next;
+void pop(void) {
+  if (top_of_stack == 0) 
+    printf("\nStack underflow");
+  else {
+    printf("\nPopped element is: %d", array_data[--top_of_stack]);
   }
 }
 
-void circular_linked_list(){
-  printf("\n*** Circular Linked List ***");
+void display_stack(void) {
+  if (top_of_stack == 0) 
+    printf("\nNo elements in the stack\n");
+  else 
+   for (int i=0; i<top_of_stack; i++) 
+     printf("%d \t", array_data[i] );
 }
 
-int main() {
-  while (1)  {
+
+void enqueue(void) {
+  if (rear == SIZE) 
+    printf("\nQueue Full");
+  else {
+    printf("\nEnter the element to be inserted to the queue: ");
+    scanf("%d", &array_data[rear++]);
+  }
+}
+
+void dequeue(void) {
+  if (front == rear) 
+    printf("\nQueue Empty");
+  else {
+    printf("\nDeleted element is: %d", array_data[front++]);
+  }
+}
+
+void display_queue(void) {
+  if (front == rear) 
+    printf("\nQueue Empty\n");
+  else 
+   for (int i=front; i<rear; i++) 
+     printf("%d \t", array_data[i] );
+}
+void array_stack() {
+  top_of_stack = 0;
+  int choice;
+  printf("\n***Creation of Stack using Array***");
+  while(1) {
+    printf("\n 1. Push \t 2. Pop \t 3. Display \t 4. Return to previous menu \n Choose an option: ");
+    scanf("%d", &choice);
+    switch (choice) {
+      case 1: push();
+            break;
+      case 2: pop();
+            break;
+      case 3: display_stack();
+            break;
+      case 4: return;
+      default: printf("\nInvalid choice");
+    }
+  }  
+}
+
+void array_queue() {
+  printf("\n***Creation of Queue using Array***");
+  front =0;
+  rear = 0;
+  int choice;
+  while(1) {
+    printf("\n 1. Enqueue \t 2. Dequeue \t 3. Display \t 4. Return to previous menu \n Choose an option: ");
+    scanf("%d", &choice);
+    switch (choice) {
+      case 1: enqueue();
+            break;
+      case 2: dequeue();
+            break;
+      case 3: display_queue();
+            break;
+      case 4: return;
+      default: printf("\nInvalid choice");
+    }
+  }
+}
+void linked_list_stack() {
+  printf("\n***Creation of Stack using Linked List***");
+}
+
+void linked_list_queue() {
+  printf("\n***Creation of Queue using Linked List***");
+}
+
+int main(void) {
+  int choice;
+  while (1){
     menu();
-    switch(choice) {
-      case 1: singly_linked_list();
+    scanf("%d", &choice);
+    switch (choice){
+      case 1: array_stack();
               break;
-      case 2: doubly_linked_list();
+      case 2: array_queue();
               break;
-      case 3: circular_linked_list();
+      case 3: linked_list_stack();
               break;
-      case 4: return 0;
-      default: printf("\nInvalid Option");
+      case 4: linked_list_queue();
+              break;
+      case 5: return 0;
+      default: printf("Invalid Option.  Try again.");
     }
-  }
     
+  }
 }
+/*
+output:
+1. Stack using Array
+2. Queue using Array
+3. Stack using Linked List
+4. Queue using Linked List
+5. Exit from the program
+
+ Provide an option: 1
+
+***Creation of Stack using Array***
+ 1. Push     2. Pop      3. Display      4. Return to previous menu 
+ Choose an option: 1
+
+Enter the element to be pushed to the stack: 3
+
+ 1. Push     2. Pop      3. Display      4. Return to previous menu 
+ Choose an option: 2
+
+Popped element is: 3
+ 1. Push     2. Pop      3. Display      4. Return to previous menu 
+ Choose an option: 4
+*/
